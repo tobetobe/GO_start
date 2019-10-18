c064618664c46df7a383573930355a384dc36ae9
tobetobe <949043032@qq.com>
2019-10-18 22:35:14 +0800

    print thing

--- hello.go
+++ hello.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world\n")
+	// fmt.Printf("hello, world\n")
+
+	var v2 = 2
+
+	var v1 string = "abc"
+
+	v3 := 10
+
+	fmt.Print(v1, v2, v3)
+
+	v3 = 500
+
+	fmt.Print(v3)
 }
