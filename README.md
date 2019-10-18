c642b08c4323d9759581d1bf4c8692e9de829551
tobetobe <949043032@qq.com>
2019-10-19 00:04:02 +0800

    daily commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # GO_start
+
+🚀go 语言学习过程中的随便练习 🚀
+
+🌞 + 没有不必要的代码 + 🌞
+
+## 学习资源 🔥
+
+- <https://github.com/unknwon/the-way-to-go_ZH_CN/>
+
+## go 中的值类型 引用类型
+
+⚠️ 和 js 中的相应内容进行对比
+
+### 1.引用类型
+
+- slices（第 7 章），maps（第 8 章）和 channel（第 13 章）
+
+### 2.值类型
+
+- 数组 结构 int string
--- hello.go
+++ hello.go
@@ -5,15 +5,42 @@ import "fmt"
 func main() {
 	// fmt.Printf("hello, world\n")
 
-	var v2 = 2
+	// var v2 = 2
 
-	var v1 string = "abc"
+	// var v1 string = "abc"
 
-	v3 := 10
+	// v3 := 10
 
-	fmt.Print(v1, v2, v3)
+	// fmt.Print(v1, v2, v3)
 
-	v3 = 500
+	// v3 = 500
 
-	fmt.Print(v3)
+	// fmt.Print(v3)
+
+	// fmt.Printf("Καλημέρα κόσμε; or こんにちは 世界\n")
+	// fmt.Print("Καλημέρα κόσμε; or こんにちは 世界\n")
+	// fmt.Println("Καλημέρα κόσμε; or こんにちは 世界")
+
+	// fmt.Print(1, 2)
+	// fmt.Printf(1, 2)
+
+	const (
+		Sunday = iota
+		Monday
+		Tuesday
+		Wednesday
+		Thursday
+		Friday
+		Saturday
+	)
+
+	fmt.Println(
+		Sunday,
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday)
+
 }
