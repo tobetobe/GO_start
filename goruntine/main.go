3ce650d2f45c666a0f97c224620e8d932436e656
tobetobe <949043032@qq.com>
2020-04-02 00:11:43 +0800

    func

--- goruntine/main.go
+++ goruntine/main.go
@@ -9,12 +9,18 @@ func send(ch chan int, i int) {
 	time.Sleep(time.Second)
 	ch <- i
 }
-
-func main() {
-	ch := make(chan int, 10)
+func run(ch chan int) {
 	for i := 0; i < 10; i++ {
 		go send(ch, i)
 	}
+}
+func sayHello() {
+	fmt.Println("Hello")
+}
+func main() {
+	ch := make(chan int, 10)
+	run(ch)
+	sayHello()
 	var list []int
 	for {
 		receive, ok := <-ch
