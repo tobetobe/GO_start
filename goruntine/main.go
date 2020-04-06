b4ea6c519f01b67c92b412cf7d9c229ecf24108f
tobetobe <949043032@qq.com>
2020-04-06 16:50:45 +0800

    etst

--- goruntine/main.go
+++ goruntine/main.go
@@ -5,31 +5,30 @@ import (
 	"time"
 )
 
-func send(ch chan int, i int) {
-	time.Sleep(time.Second)
-	ch <- i
-}
-func run(ch chan int) {
-	for i := 0; i < 10; i++ {
-		go send(ch, i)
-	}
-}
-func sayHello() {
-	fmt.Println("Hello")
-}
-func main() {
-	ch := make(chan int, 10)
-	run(ch)
-	sayHello()
-	var list []int
-	for {
-		receive, ok := <-ch
-		fmt.Println(ok)
-		list = append(list, receive)
-		fmt.Println(list)
-		if len(list) == 10 {
-			break
-		}
-	}
+// func send(ch chan int, i int) {
+// 	time.Sleep(time.Second)
+// 	ch <- i
+// }
+// func run(ch chan int) {
+// 	for i := 0; i < 10; i++ {
+// 		go send(ch, i)
+// 	}
+// }
+// func sayHello() {
+// 	fmt.Println("Hello")
+// }
+// func main() {
+// 	ch := make(chan int)
+// 	send(ch, 2)
+// 	<-ch
+// 	fmt.Println("123")
 
+// }
+
+func main() {
+	go func() {
+		time.Sleep(time.Second)
+		fmt.Println("hh")
+	}()
+	// time.Sleep(2 * time.Second)
 }
